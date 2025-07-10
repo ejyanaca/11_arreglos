1f35bd0514c46db80c37517f7f00f4bf4d475573
AlonsoVasquezDA <alonsovasquez1409@gmail.com>
2025-07-09 22:55:20 -0500

    colocamos una condicional que no permita al usuario ingresar un numero fuera del arreglo y mejoramos la estructura de la ejecucion

--- arreglos3.cpp
+++ arreglos3.cpp
@@ -4,19 +4,21 @@ int main(){
     int n,aux,A[20];
     cout<<"Ingrese la cantidad de elementos del arreglo: ";
     cin>>n;
+    if (n<1 || n>20){
+        cout<<"Ingreso un numero fuera del rango ."<<endl;
+    }
     for(int i=0; i<n; i++){
-        cout<<"Digite el elemento "<<i+1<<" del arreglo: ";
+        cout<<"Digite el elemento "<< i+1 <<": ";
         cin>>A[i];
     }
-    cout<<endl;
     for(int i=0; i<n/2; i++){
         aux=A[i];
         A[i]=A[n-i-1];
         A[n-i-1]=aux;
     }
-    cout<<endl;
+    cout<< "\nArreglo invertido:\n";
     for (int i=0; i<n; i++){
-        cout<<A[i]<<endl;
+        cout<<A[i]<< " ";
     }
     return 0;     
 }
