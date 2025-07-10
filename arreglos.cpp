d62be6174f736fff29e12faf42d018876e8d0020
AdairSandoval2025 <Adair.j98232@gmail.com>
2025-07-09 21:39:04 -0500

    se le agrego una variable(pos) que identifica la posicion del numero mayor

--- arreglos.cpp
+++ arreglos.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 using namespace std; 
 int main(){
-int n,max,A[20];
+int n,max,A[20],pos;
+pos=0;
+cout<<"Ingrese la cantidad de Numeros: ";
 cin>>n;
 for(int i=0; i<n; i++){
+    cout<<"Ingrese el numero "<<i+1<<": ";
     cin>>A[i];
 }
 max=A[0];
 for(int i=1; i<n; i++){
     if(A[i]>max){
         max=A[i];
+        pos=i;
     }
 }
-cout<<max;
+cout<<"El numero maximo es:"<<max<<" y se encuentra en a posicion "<<pos+1<<"."<<endl;
 return 0;
 }
 
